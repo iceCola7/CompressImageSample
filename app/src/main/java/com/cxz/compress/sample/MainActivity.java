59e147999bf116511e5d8ac29063a5835df32598
chenxz <563859095@qq.com>
2021-09-25 20:06:40 +0800

    update androidx

--- app/src/main/java/com/cxz/compress/sample/MainActivity.java
+++ app/src/main/java/com/cxz/compress/sample/MainActivity.java
@@ -7,8 +7,8 @@ import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
-import android.support.v7.app.AppCompatActivity;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 
--- compresslib/src/main/java/com/cxz/compresslib/utils/CachePathUtil.java
+++ compresslib/src/main/java/com/cxz/compresslib/utils/CachePathUtil.java
@@ -1,13 +1,6 @@
 package com.cxz.compresslib.utils;
 
-import android.content.ContentUris;
-import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Environment;
-import android.provider.DocumentsContract;
-import android.provider.MediaStore;
 
 import java.io.File;
 import java.text.SimpleDateFormat;
